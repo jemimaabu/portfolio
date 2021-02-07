59016ca30d30f42cafa7d57d8051941df1bdffae
jemimaabu <jemimaabu@gmail.com>
2021-02-07 19:09:17 +0000

    updated layout and logo

---
+++ index.js
@@ -0,0 +1,16 @@
+const getTheme = () => {
+  var root = document.getElementsByTagName('html')[0];
+  const theme = localStorage.getItem('theme') === null ? 'light' : localStorage.getItem('theme');
+  root.className = theme
+}
+
+const setTheme = (className) => {
+  var root = document.getElementsByTagName('html')[0]
+  root.className = '';
+  root.classList.add(className);
+  localStorage.setItem('theme', className)
+}
+
+window.onload = () => {
+  getTheme()
+}
