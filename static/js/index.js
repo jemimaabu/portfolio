5848fc4503ac77267016fe8d0468d8c043a22853
jemimaabu <jemimaabu@gmail.com>
2021-02-16 10:55:00 +0000

    updated scroll

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,22 @@
-const themeDisplay = document.getElementById('theme-display');
+const root = document.querySelector('html');
+const body = document.querySelector('body');
+const menu = document.querySelector('.nav-links');
+const menuButton = document.getElementById('menu-display'); const themeDisplay = document.getElementById('theme-display');
 const themeContainer = document.querySelector('.theme-container');
 const themeSelectors = document.getElementsByClassName('theme-select');
-const root = document.getElementsByTagName('html')[0];
-const menu = document.querySelector('.nav-links');
-const menuButton = document.getElementById('menu-display');
 
 const getTheme = () => {
   const theme = localStorage.getItem('theme') === null ? 'light' : localStorage.getItem('theme');
   setActiveSelector(theme);
   root.className = theme
+}
+
+const setTheme = (className) => {
+  var root = document.getElementsByTagName('html')[0];
+  root.className = '';
+  root.classList.add(className);
+  setActiveSelector(className);
+  localStorage.setItem('theme', className)
 }
 
 const setActiveSelector = (className) => {
@@ -24,49 +32,45 @@ const showThemeContainer = () => {
   themeContainer.classList.add('visible');
   [...themeSelectors].forEach(item => {
     item.tabIndex = 0
-  })
+  });
+  body.classList.add('no-scroll');
 }
 
 const hideThemeContainer = () => {
   themeContainer.classList.remove('visible');
   [...themeSelectors].forEach(item => {
     item.tabIndex = -1
-  })
-}
-
-themeDisplay.addEventListener("click", function () {
-  hideMenu()
-  if (themeContainer.classList.contains('visible')) {
-    hideThemeContainer()
-  } else {
-    showThemeContainer()
-  }
-})
-
-const setTheme = (className) => {
-  var root = document.getElementsByTagName('html')[0];
-  root.className = '';
-  root.classList.add(className);
-  setActiveSelector(className);
-  localStorage.setItem('theme', className)
+  });
+  body.classList.remove('no-scroll');
 }
 
 const showMenu = () => {
   menu.classList.add('visible');
   menuButton.classList.add('active');
+  body.classList.add('no-scroll');
 }
 
 const hideMenu = () => {
   menu.classList.remove('visible');
   menuButton.classList.remove('active');
+  body.classList.remove('no-scroll');
 }
+
+themeDisplay.addEventListener("click", function () {
+  hideMenu()
+  if (themeContainer.classList.contains('visible')) {
+    hideThemeContainer();
+  } else {
+    showThemeContainer();
+  }
+})
 
 menuButton.addEventListener("click", function () {
   hideThemeContainer();
   if (menu.classList.contains('visible')) {
     hideMenu();
   } else {
-    showMenu()
+    showMenu();
   }
 })
 
