58ae1d63a9bd38e98f18a025c47828b41506ce73
jemimaabu <jemimaabu@gmail.com>
2021-02-21 13:14:13 +0000

    included menu scroll

---
+++ static/js/helpers.js
@@ -0,0 +1,14 @@
+let previousScrollPosition = 0;
+
+export const isScrollingDown = () => {
+  let scrolledPosition = window.scrollY;
+  let isScrollDown;
+
+  if (scrolledPosition > previousScrollPosition) {
+    isScrollDown = true;
+  } else {
+    isScrollDown = false;
+  }
+  previousScrollPosition = scrolledPosition;
+  return isScrollDown;
+}
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,6 @@
 const root = document.querySelector('html');
 const body = document.querySelector('body');
+const mainNav = document.querySelector("nav");
 const menu = document.querySelector('.nav-links');
 const menuButton = document.getElementById('menu-display'); const themeDisplay = document.getElementById('theme-display');
 const themeContainer = document.querySelector('.theme-container');
@@ -56,6 +57,36 @@ const hideMenu = () => {
   body.classList.remove('no-scroll');
 }
 
+let previousScrollPosition = 0;
+
+const isScrollingDown = () => {
+  let scrolledPosition = window.scrollY;
+  let isScrollDown;
+
+  if (scrolledPosition > previousScrollPosition) {
+    isScrollDown = true;
+  } else {
+    isScrollDown = false;
+  }
+  previousScrollPosition = scrolledPosition;
+  return isScrollDown;
+}
+
+const handleNavScroll = () => {
+  if (mainNav.classList.contains('visible')) {
+    if (isScrollingDown()) {
+      mainNav.classList.add('scroll-down');
+      mainNav.classList.remove('scroll-up')
+    } else {
+      mainNav.classList.add('scroll-up');
+      mainNav.classList.remove('scroll-down')
+    }
+  } else {
+    mainNav.classList.remove('scroll-up');
+    mainNav.classList.remove('scroll-down')
+  }
+}
+
 themeDisplay.addEventListener("click", function () {
   hideMenu()
   if (themeContainer.classList.contains('visible')) {
@@ -72,6 +103,10 @@ menuButton.addEventListener("click", function () {
   } else {
     showMenu();
   }
+})
+
+window.addEventListener('scroll', () => {
+  handleNavScroll()
 })
 
 window.addEventListener('load', () => {
