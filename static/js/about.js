52360d5dcb700ffd62f6b1d54a63691ec3aed05f
jemimaabu <jemimaabu@gmail.com>
2021-03-24 17:56:22 +0000

    updated about page

---
+++ static/js/about.js
@@ -0,0 +1,11 @@
+
+const openDetails = () => {
+  console.log(!!window.location.hash)
+  if (!!window.location.hash) {
+    const details = document.querySelector(`${window.location.hash} details`)
+    details.open = true;
+  }
+}
+
+openDetails();
+window.addEventListener("hashchange", openDetails);
